f10cae775d618021594fd6cbd3335840e773139b
Akanksha <akankshasaini03@gmail.com>
2020-08-27 14:04:42 +0530

    main method added

---
+++ main.py
@@ -0,0 +1,48 @@
+import sys
+def match_pattern(pattern, text):
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+if __name__ == "__main__":
+	length_of_args = len(sys.argv)
+	args_list = str(sys.argv)
+	result = match_pattern(args_list[0], args_list[1])
+	print(result)
+
