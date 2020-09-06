1e0a0407f3cbb30c0ea7455884acff26e01aec5c
Akanksha <akankshasaini03@gmail.com>
2020-09-06 13:27:02 +0530

    added groups to solve regex

--- regex.py
+++ regex.py
@@ -8,6 +8,43 @@ alpha_numeric = small_alphabet + capital_alphabet + digits + '_'
 groups = {"A-Z": set(capital_alphabet), "a-z": set(small_alphabet), "0-9": set(digits)}
 backward_slash_group = {"\d": set(digits), "\w": set(alpha_numeric)}
 special_pattern = {"(": "solve_parentheese", "[": "solve_square_bracket", "\\": "solve_backward_slash"}
+operators = {"+", "*"}
+
+
+# def set_for_operators(pattern, text):
+# 	chars = set()
+# 	pattern_ind = 2
+# 	if pattern[0] == "\\":
+# 		pattern_ind = 3
+# 		if pattern[:2] in backward_slash_group:
+# 			chars.update(backward_slash_group[pattern[:2]])
+# 		else:
+# 			chars.update(pattern[1])
+# 	else:
+# 		chars.add(pattern[0])
+# 	return pattern_ind, chars
+
+
+# def solve_plus_operator(pattern, text):
+# 	pointer = 0
+# 	pattern_ind, chars = set_for_operators(pattern, text)
+	
+# 	while pointer < len(text) and text[pointer] in chars:
+# 		pointer += 1
+# 	index = pointer
+
+# 	if pointer == len(text) and pattern_ind+1 == len(pattern):
+# 		return True, (pointer, pattern_ind)
+
+# 	while pointer != -1:
+# 		if find_pattern(pattern[pattern_ind:], text[pointer:]) != -1:
+# 			return True, (pointer, pattern_ind)
+# 		pointer -= 1
+
+# 	if index >= 1:
+# 		return True, (index, pattern_ind)	
+# 	return False, (-1, -1)
+
 
 
 
@@ -31,108 +68,183 @@ def find_closing_bracket(string: str, opening_loc: int) -> int:
 		raise ValueError("Unmatched opening bracket.")
 
 
-def solve_parenthese(pattern: str, text: str) -> set:
-	ending_brack = find_closing_bracket(pattern, 0)
-	new_pattern = pattern[1:ending_brack]
-	index = find_pattern(new_pattern, text)
-	if index == -1:
-		return False, (-1, -1)
-	return True, (index+1, ending_brack+1)
+# def solve_parenthese(pattern: str, text: str) -> set:
+# 	ending_brack = find_closing_bracket(pattern, 0)
+# 	new_pattern = pattern[1:ending_brack]
+# 	index = find_pattern(new_pattern, text)
+# 	if index == -1:
+# 		return False, (-1, -1)
+# 	return True, (index+1, ending_brack+1)
 
 
-def simplify_square_bracket(pattern: str) -> set:
-	if len(pattern) == 0:
-		return set()
-	i = 0
-	chars = set()
-	if pattern[0] == "^":
-		i = 1
+# def simplify_square_bracket(pattern: str) -> set:
+# 	if len(pattern) == 0:
+# 		return set()
+# 	i = 0
+# 	chars = set()
+# 	if pattern[0] == "^":
+# 		i = 1
 	
+# 	while i < len(pattern):
+# 		if i+2 < len(pattern) and pattern[i:i+3] in groups:
+# 			chars.update(groups[pattern[i:i+3]])
+# 			i = i+3
+# 		elif pattern[i] == "\\":
+# 			if i+1 == len(pattern):
+# 				raise ValueError("Dangling backslash")
+# 			if pattern[i:i+2] in backward_slash_group:
+# 				chars.update(backward_slash_group[pattern[i:i+2]])
+# 				i += 2
+# 			elif i+3 < len(pattern) and pattern[i+1: i+4] in groups:
+# 				chars.update(groups[pattern[i+1: i+4]])
+# 				i += 4
+# 			else:
+# 				chars.update({pattern[i+1]})
+# 				i += 2
+			
+# 		else:
+# 			chars.add(pattern[i])
+# 			i += 1
+
+# 	return chars
+
+
+# def solve_square_bracket(pattern: str, text: str) -> tuple:
+# 	ending_brack =find_closing_bracket(pattern, 0)
+# 	if ending_brack+1 < len(pattern) and pattern[ending_brack+1] in operators:
+# 		return solve_operators(pattern, text)
+# 	chars = simplify_square_bracket(pattern[1:ending_brack])
+# 	if pattern[1] == "^":
+# 		if text[0] not in chars:
+# 			return True, (1, ending_brack+1)
+		
+# 	else:
+# 		if text[0] in chars:
+# 			return True, (1, ending_brack+1)
+
+# 	return False, (-1, -1)
+
+
+
+# def solve_backward_slash(pattern, text):
+# 	if len(pattern) == 1:
+# 		raise ValueError("Dangling backslash")
+# 	if len(pattern) >= 3 and pattern[2] in operators:
+# 		return solve_operators(pattern, text)
+# 	elif (pattern[:2] in backward_slash_group) and (text[0] in backward_slash_group[pattern[:2]]):
+# 		return True, (1, 2)
+# 	elif text[0] == pattern[1]:
+# 		return True, (1, 2)
+# 	return False, (-1, -1)
+	
+
+
+# def solve_special_pattern(pattern: str, text: str) -> tuple:
+# 	if pattern[0] == "[":
+# 		return solve_square_bracket(pattern, text)
+# 	elif pattern[0] == "(":
+# 		return solve_parenthese(pattern, text)
+# 	elif pattern[0] == "\\":
+# 		return solve_backward_slash(pattern, text)
+
+
+# def find_pattern(pattern: str, text: str) -> int:
+# 	i = 0
+# 	j = 0
+# 	while i < len(pattern) and j < len(text):
+# 		if pattern[i] in special_pattern:
+# 			is_match, indexes = solve_special_pattern(pattern[i:], text[j:])
+# 			if not is_match:
+# 				return -1
+# 			i += indexes[1]
+# 			j += indexes[0]
+
+# 		elif i+1 < len(pattern) and pattern[i+1] in operators:
+# 			is_match, indexes = solve_operators(pattern[i:], text[j:])
+# 			if not is_match:
+# 				return -1
+# 			i += indexes[1]
+# 			j += indexes[0]
+
+
+# 		elif pattern[i] == ")":
+# 			raise ValueError("Unmatched closing bracket")
+
+# 		elif pattern[i] == text[j]:
+# 			i += 1
+# 			j += 1
+
+# 		else:
+# 			break
+
+# 	if i != len(pattern):
+# 		return -1
+# 	return j - 1
+
+def match_regex_groups(groups: list, text: str) -> int:
+	text_pointer = 0
+	for i,pattern in enumerate(groups):
+		if pattern[-1] in operators:
+			index = solve_operators(pattern, text)
+		elif pattern[i] == text[text_pointer]:
+			text_pointer += 1
+	print(i)
+	return text_pointer - 1
+
+
+
+
+
+
+
+def check_for_operator(pattern: str, i: int) -> int:
+	if i+1 == len(pattern) or pattern[i+1] not in operators:
+		return 0
+	return 1
+
+
+def break_regex(pattern: str) -> list:
+	i = 0
+	groups = []
 	while i < len(pattern):
-		if i+2 < len(pattern) and pattern[i:i+3] in groups:
-			chars.update(groups[pattern[i:i+3]])
-			i = i+3
+		if pattern[i] in bracket_pair:
+			closing_bracket = find_closing_bracket(pattern, i)
+			is_operator = check_for_operator(pattern, closing_bracket)
+			groups.append(pattern[i: closing_bracket+1+is_operator])
+			i = (closing_bracket+1+is_operator)
+
 		elif pattern[i] == "\\":
 			if i+1 == len(pattern):
-				raise ValueError("Dangling backslash")
-			if pattern[i:i+2] in backward_slash_group:
-				chars.update(backward_slash_group[pattern[i:i+2]])
-			else:
-				chars.update({pattern[i+1]})
-			i += 2
-			
+				raise ValueError("Dangling backslash.")
+			is_operator = check_for_operator(pattern, i+1)
+			groups.append(pattern[i: i+2+is_operator])
+			i += (2 + is_operator)
+
+		elif pattern[i] == ")":
+			raise ValueError("Unmatched closing bracket.")
+
 		else:
-			chars.add(pattern[i])
-			i += 1
+			is_operator = check_for_operator(pattern, i)
+			groups.append(pattern[i: i+1+is_operator])
+			i += (1 + is_operator)
 
-	return chars
-
-
-def solve_square_bracket(pattern: str, text: str) -> tuple:
-	ending_brack =find_closing_bracket(pattern, 0)
-	chars = simplify_square_bracket(pattern[1:ending_brack])
-	if pattern[1] == "^":
-		if text[0] not in chars:
-			return True, (1, ending_brack+1)
-		
-	else:
-		if text[0] in chars:
-			return True, (1, ending_brack+1)
-
-	return False, (-1, -1)
+	return groups
 
 
-
-def solve_backward_slash(pattern, text):
-	if len(pattern) == 1:
-		raise ValueError("Dangling backslash")
-	if (pattern[:2] in backward_slash_group) and (text[0] in backward_slash_group[pattern[:2]]):
-		return True, (1, 2)
-	elif text[0] == pattern[1]:
-		return True, (1, 2)
-	return False, (-1, -1)
-	
+def match_regex(pattern: str, text: str) -> int:
+	groups = break_regex(pattern)
+	return match_regex_groups(groups, text)
 
 
-def solve_special_pattern(pattern: str, text: str) -> tuple:
-	if pattern[0] == "[":
-		return solve_square_bracket(pattern, text)
-	elif pattern[0] == "(":
-		return solve_parenthese(pattern, text)
-	elif pattern[0] == "\\":
-		return solve_backward_slash(pattern, text)
-
-
-def find_pattern(pattern: str, text: str) -> int:
-	i = 0
-	j = 0
-	while i < len(pattern) and j < len(text):
-		if pattern[i] in special_pattern:
-			is_match, indexes = solve_special_pattern(pattern[i:], text[j:])
-			if not is_match:
-				return -1
-			i += indexes[1]
-			j += indexes[0]
-
-		elif pattern[i] == text[j]:
-			i += 1
-			j += 1
-		else:
-			break
-
-	if i != len(pattern):
-		return -1
-	return j - 1
-
-
-
-def match_pattern(pattern: str, text: str) -> list:
-	result = set()
+def find_pattern(pattern: str, text: str) -> list:
+	groups = break_regex(pattern)
+	result = []
 	for i in range(len(text)):
-		index = find_pattern(pattern, text[i:])
+		index = match_regex_groups(groups, text)
 		if index != -1:
-			result.add(text[i:i+index+1])
+			result.append(text[i:index+1])
 	return result
+
 
 
 
--- test_basic.py
+++ test_basic.py
@@ -6,33 +6,64 @@ digits = "0123456789"
 alpha_numeric = small_alphabet + capital_alphabet + digits + '_'
 
 class TestBasic(unittest.TestCase):
+	def test_break_regex(self):
+		self.assertEqual(rx.break_regex(r"\s+(as+)+[a-z]+"), ["\s+", "(as+)+", "[a-z]+"])
 
-	def test_find_pattern(self):
-		self.assertEqual(rx.find_pattern("abc", "bc"), -1)
-		self.assertEqual(rx.find_pattern("[a-z][0-9]c", "a5ccs"), 2)
-		self.assertEqual(rx.find_pattern("abc", "abc"), 2)
-		self.assertEqual(rx.find_pattern("[]c", "acsdf"), -1)
-		self.assertEqual(rx.find_pattern("xyfggh", "xyz"), -1)
-
-	def test_match_pattern(self):
-		self.assertEqual(rx.match_pattern(r"x[^s3+][a-z]", "7xas4-"), {"xas"})
-		self.assertEqual(rx.match_pattern(r"[7]x","77x"), {"7x"})
-		self.assertEqual(rx.match_pattern(r"[\da]e\w", "7ee8 2emaec"), {"2em", "7ee", "aec"})
-		self.assertEqual(rx.match_pattern(r"((a)[^a-z])", "abcaL"), {"aL"})
-		self.assertEqual(rx.match_pattern(r"[a-z\d\\\]]", "[]6\e"), {"6", "e", "]", "\\"})
-		self.assertEqual(rx.match_pattern(r"[A-Z][a-z]+", "+eAh+fKa+"), {"Ka+", "Ah+"})
+	def find_pattern(self):
+		self.assertEqual(rx.find_pattern(r"sdc", "sdcdfsdc"), ["sdc", "sdc"])
 
 
-	def test_find_closing_bracket(self):
-		self.assertEqual(rx.find_closing_bracket("(\())", 0), 3)
-		self.assertEqual(rx.find_closing_bracket("([(())])", 1), 6)
-		self.assertEqual(rx.find_closing_bracket("[(()]", 0), 4)
-		self.assertEqual(rx.find_closing_bracket("(([))", 1), 3)
 
-	def test_simply_square_bracket(self):
-		self.assertEqual(rx.simplify_square_bracket("A-Z"), set(capital_alphabet))
-		self.assertEqual(rx.simplify_square_bracket("a+/a-z"), set(small_alphabet + '+' + '/'))
-		self.assertEqual(rx.simplify_square_bracket("0-9A-Z"), set(digits+capital_alphabet))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	# def test_find_pattern(self):
+	# 	self.assertEqual(rx.find_pattern("abc", "bc"), -1)
+	# 	self.assertEqual(rx.find_pattern("[a-z][0-9]c", "a5ccs"), 2)
+	# 	self.assertEqual(rx.find_pattern("abc", "abc"), 2)
+	# 	self.assertEqual(rx.find_pattern("[]c", "acsdf"), -1)
+	# 	self.assertEqual(rx.find_pattern("xyfggh", "xyz"), -1)
+
+	# def test_match_pattern(self):
+	# 	self.assertEqual(rx.match_pattern(r"[\\a-z]", r"\he"), {"h", "e", "\\"})
+	# 	self.assertEqual(rx.match_pattern(r"x[^s3+][a-z]", "7xas4-"), {"xas"})
+	# 	self.assertEqual(rx.match_pattern(r"\w+\a\6\5\7", "baaa657"), {"baaa657"})
+	# 	self.assertEqual(rx.match_pattern(r"[\da]e\w", "7ee8 2emaec"), {"2em", "7ee", "aec"})
+	# 	self.assertEqual(rx.match_pattern(r"[a-z\d\\\]]", "[]6\e"), {"6", "e", "]", "\\"})
+	# 	self.assertEqual(rx.match_pattern(r"[A-Z][a-z]+", "+eAh+fKa+"), {"Ka+", "Ah+"})
+	# 	self.assertEqual(rx.match_pattern(r"\d+", "klsd6fsl"), {"6"})
+
+	# def test_find_closing_bracket(self):
+	# 	self.assertEqual(rx.find_closing_bracket("(\())", 0), 3)
+	# 	self.assertEqual(rx.find_closing_bracket("([(())])", 1), 6)
+	# 	self.assertEqual(rx.find_closing_bracket("[(()]", 0), 4)
+	# 	self.assertEqual(rx.find_closing_bracket("(([))", 1), 3)
+
+	# def test_simply_square_bracket(self):
+	# 	self.assertEqual(rx.simplify_square_bracket("A-Z"), set(capital_alphabet))
+	# 	self.assertEqual(rx.simplify_square_bracket("a+/a-z"), set(small_alphabet + '+' + '/'))
+	# 	self.assertEqual(rx.simplify_square_bracket("0-9A-Z"), set(digits+capital_alphabet))
                                                     
 
 if __name__ == "__main__":
